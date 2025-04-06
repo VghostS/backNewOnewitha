26e3ddb08034ae559451c8f4347552660407c002
VghostS <vGHOSTs0571@gmail.com>
2025-04-07 00:48:58 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,16 @@
 const time = require('./utils/timeUtils');
+
 const authRoutes = require('./routers/authRoutes');
 const paymentRoutes = require('./routers/paymentRoutes');
 const generalRoutes = require('./routers/generalRoutes');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors'); // Add this line
 
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 1000;
-
-// Configure CORS
-app.use(cors({
-  origin: ['https://vghosts.github.io', 'http://localhost:3000'], // Add your frontend URLs
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 app.use(bodyParser.json());
 
